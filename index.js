eb25a541ff5482182d33b75e7409b08ffbcb9550
lemuelnjuki <lemuel.njuki@student.moringaschool.com>
2022-08-09 10:23:50 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const main = document.getElementById('main')
+main.remove();
+const newHeader = document.getElementById('victory');
+newHeader.innerHTML = 'Lemuel is the champion'
+
